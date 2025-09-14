f5f9513689ca7370217b90441ea248abfd45f72b
Salem Nassar <Salemnassar5@gmail.com>
2025-09-14 10:31:06 -0500

    Telescope searches and greps dotfiles and gitignored files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,10 +2,26 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
+    opts = {
+      pickers = {
+        find_files = {
+          find_command = { "fd", "--type", "f", "--hidden", "--no-ignore", "--exclude", ".git" },
+        },
+        live_grep = {
+          additional_args = { "--hidden", "--no-ignore", "--glob", "!.git/" },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
       local telescope = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Find Files" })
       vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Live Grep" })
+      require("which-key").add({
+        { "<leader>f", group = "Find" },
+        { "<leader>ff", desc = "Find Files (all files)" },
+        { "<leader>fg", desc = "Live Grep (all files)" },
+      })
     end,
   },
 }
